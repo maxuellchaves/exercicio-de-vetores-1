d252ea159b863c8e1be5d4a23640ecea341565b6
Maxuell Chaves <maxuellchaves30@gmail.com>
2020-04-17 22:56:25 -0300

    Programa finalizado

---
+++ Aluguel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exerc_vect_1
+{
+    class Aluguel
+    {
+        public string  Name { get; set; }
+        public string Email { get; set; }
+        public int  Quartos { get; set; }
+
+        public Aluguel(string name, string email, int quartos)
+        {
+            Name = name;
+            Email = email;
+            Quartos = quartos;
+        }
+
+
+        public override string ToString()
+        {
+            StringBuilder os = new StringBuilder();
+            os.Append(Quartos+": ");
+            os.Append(Name + ", ");
+            os.Append(Email);
+
+            return os.ToString();
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exerc_vect_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos quartos serão alugados? ");
+            int n = int.Parse(Console.ReadLine());
+            Aluguel[] vect = new Aluguel[10];
+            Console.WriteLine();
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Aluguel #{i}: ");
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quartos: ");
+                int quartos = int.Parse(Console.ReadLine());
+               
+
+                vect[quartos] = new Aluguel(name, email, quartos);
+                Console.WriteLine();
+
+            }
+
+
+            Console.WriteLine("Quartos ocupados: ");
+            foreach ( Aluguel obj in vect)
+            {
+                if (obj != null)
+                {
+                    Console.WriteLine(obj);
+                }
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
